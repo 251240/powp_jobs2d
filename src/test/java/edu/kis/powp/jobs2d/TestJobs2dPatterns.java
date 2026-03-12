21bff51f6396e8da84aa8053f301cea3c5369215
kw\kamil <251240@edu.p.lodz.pl>
2026-03-12 01:51:54 +0100

    Wykonanie zadania 3.3.2, dodanie mozliwosci kozystania z LineDrawerAdapter w testach

--- src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
+++ src/test/java/edu/kis/powp/jobs2d/TestJobs2dPatterns.java
@@ -13,6 +13,8 @@ import edu.kis.powp.jobs2d.events.SelectChangeVisibleOptionListener;
 import edu.kis.powp.jobs2d.events.SelectTestFigureOptionListener;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
+import edu.kis.powp.jobs2d.drivers.adapter.LineDrawerAdapter;
+import edu.kis.legacy.drawer.shape.LineFactory;
 
 public class TestJobs2dPatterns {
 	private final static Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
@@ -42,6 +44,10 @@ public class TestJobs2dPatterns {
 
 		Job2dDriver testDriver = new DrawerAdapter(DrawerFeature.getDrawerController());
 		DriverFeature.addDriver("Buggy Simulator", testDriver);
+
+		Job2dDriver specialDriver = new LineDrawerAdapter(DrawerFeature.getDrawerController(),
+						LineFactory.getSpecialLine());
+		DriverFeature.addDriver("Special Line Driver", specialDriver);
 
 		DriverFeature.updateDriverInfo();
 	}
